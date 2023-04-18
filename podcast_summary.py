7afaca6042f3fd763b8d1d8c0f68644b8fc42f0b
Hoo <sksk8922@gmail.com>
2023-04-19 02:11:06 +0900

    make get_episodes task

--- podcast_summary.py
+++ podcast_summary.py
@@ -9,20 +9,23 @@ import xmltodict
 
 @dag(
     dag_id="podcast_summary",
-    schedule_interval="@weekly",
+    schedule_interval="@hourly",
     start_date=pendulum.datetime(2023, 4, 18),
     catchup=False,
 )
 # creating our first dataline
 # comtain all the logic of pipline
 def podcast_summary():
-    # create operator
+    # create operator s s
     @task()
     def get_episodes():
         data = requests.get("https://marketplace.org/feed/podcast/marketplace")
         feed = xmltodict.parse(data.text)
-        episodes = feed["rss"]["channel"]["items"]
+        episodes = feed["rss"]["channel"]["item"]
         print(f"Found {len(episodes)} episodes.")
         return episodes
 
     podcast_episodes = get_episodes()
+
+
+summary = podcast_summary()
