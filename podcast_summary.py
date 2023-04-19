9b4b63a7882070a564cd8d9f1051d0f30e12291a
Hoo <sksk8922@gmail.com>
2023-04-19 22:03:08 +0900

    make create databses task

--- podcast_summary.py
+++ podcast_summary.py
@@ -5,8 +5,12 @@ import requests
 import xmltodict
 
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 
 # Download podcast metadata
+
+
+PODCAST_URL = "https://marketplace.org/feed/podcast/marketplace"
 
 
 # all DAGs property
@@ -20,29 +24,62 @@ from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 # contain all the logic of pipeline
 # you should change airflow.cfg dags_folder
 def podcast_summary():
-    create_databases = SqliteOperator(
+    create_database = SqliteOperator(
         task_id="create_table_sqlite",
         sql=r"""
-        CREATE TABLE IF NOT EXISTS episodes(
+        CREATE TABLE IF NOT EXISTS episodes (
             link TEXT PRIMARY KEY,
             title TEXT,
             filename TEXT,
             published TEXT,
             description TEXT
-        )
+        );
         """,
+        sqlite_conn_id="podcasts",
     )
 
+    # Decoration turns function into Airflow Task
     @task()
     def get_episodes():
-        data = requests.get("https://marketplace.org/feed/podcast/marketplace")
+        data = requests.get(PODCAST_URL)
         feed = xmltodict.parse(data.text)
         episodes = feed["rss"]["channel"]["item"]
         print(f"Found {len(episodes)} episodes.")
         return episodes
 
     podcast_episodes = get_episodes()
-    create_databases.set_downstream(podcast_episodes)
+    create_database.set_downstream(podcast_episodes)
 
 
 summary = podcast_summary()
+# @task()
+# def load_episodes(episodes):
+#     hook = SqliteHook(sqlite_conn_id="podcasts")
+#     stored = hook.get_pandas_df("SELECT * from episodes;")
+#     new_episodes = []
+#     for episode in episodes:
+#         if episode["link"] not in stored["link"].values:
+#             filename = f"{episode['link'].split('/')[-1]}.smp3"
+#             new_episodes.append(
+#                 [
+#                     episode["link"],
+#                     episode["titlde"],
+#                     episode["pubDate"],
+#                     episode["description"],
+#                     filename,
+#                 ]
+#             )
+
+#     hook.insert_rows(
+#         table="episodes",
+#         rows=new_episodes,
+#         target_fields=[
+#             "link",
+#             "title",
+#             "published",
+#             "description",
+#             "filename",
+# ],
+#     )
+
+# load_episodes(podcast_episodes)
